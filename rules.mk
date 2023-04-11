e6323a01e513cf3261193d1561614debae895655
Salvador Vicente <svicente@iberdrola.es>
2023-04-11 10:45:16 +0200

    Apago leds y oled

--- rules.mk
+++ rules.mk
@@ -1,5 +1,5 @@
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = no
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
 
